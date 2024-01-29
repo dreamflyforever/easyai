29a87aa7323f35ecb7efd41f07dd6e0e3e2badc1
dreamflyforever <1150660744@qq.com>
2024-01-30 00:57:35 +0800

    integrated adb in python
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- display/show.py
+++ display/show.py
@@ -1,14 +1,19 @@
 import cv2
 import time
+import subprocess
+import os
 def main():
     i=0
     while(1):
-        frame = cv2.imread(str(i) + "_out.jpeg")
+        img = str(i) + "_out.jpeg"
+        #subprocess.call("adb pull /oem/ws/" + img)
+        os.system("adb pull /oem/ws/" + img)
+        frame = cv2.imread(img)
         cv2.imshow('capture', frame)
         i=i+1
         if (i == 50):
             i = 0
-        if cv2.waitKey(100)&0xFF==ord('q'):#按键盘q就停止拍照
+        if cv2.waitKey(1)&0xFF==ord('q'):#按键盘q就停止拍照
             break
     cv2.destroyAllWindows()
 
