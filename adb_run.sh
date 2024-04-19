b0289f73758a9eea9f9bac9569d062c5bf3330e0
dreamflyforever <1150660744@qq.com>
2024-04-19 15:34:47 +0800

    update lib
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- adb_run.sh
+++ adb_run.sh
@@ -7,6 +7,7 @@ adb push install/rv1106_linux_armhf/rknn__demo/model/coco_80_labels_list.txt /oe
 adb push 3rdparty/alsa/share/alsa/alsa.conf /oem/ws/share/alsa/alsa.conf
 adb push install/rv1106_linux_armhf/rknn__demo/model/yolov5s-640-640.rknn /userdata/
 adb push RkLunch.sh /oem/usr/bin/RkLunch.sh
+adb push lib/librknnmrt.* /oem/usr/lib/
 adb shell reboot
 sleep 10
 cd display; python show.py
