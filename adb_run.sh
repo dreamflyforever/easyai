dfe6b82f7ee4a3fed89f363ad1c6343ac9096184
dreamflyforever <1150660744@qq.com>
2024-05-27 17:10:18 +0800

    update rknn sleep_v71 version
    train gray model
    
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- adb_run.sh
+++ adb_run.sh
@@ -11,15 +11,20 @@ adb push install/rv1106_linux_armhf/rknn__demo/model/welcome.wav /oem/ws/model
 adb push install/rv1106_linux_armhf/rknn__demo/model/labels_list.txt /oem/ws/model
 
 adb push kernel/model/leftright.wav /oem/ws/model
-adb push kernel/model/child.wav /oem/ws/model
 adb push kernel/model/mobile.wav /oem/ws/model
-adb push kernel/model/talking.wav /oem/ws/model
 adb push kernel/model/verify.wav /oem/ws/model
 
+adb push ertong/* /oem/ws/model/
+
 adb push 3rdparty/alsa/share/alsa/alsa.conf /oem/ws/share/alsa/alsa.conf
-adb push install/rv1106_linux_armhf/rknn__demo/model/sleep_v55.rknn /userdata/
+#adb push install/rv1106_linux_armhf/rknn__demo/model/sleep_v55.rknn /userdata/
+adb push rm /userdata/sleep_v69.rknn 
+adb push sleep_v71/weights/sleep_v71.rknn /userdata/
 adb push RkLunch.sh /oem/usr/bin/RkLunch.sh
 adb push lib/librknnmrt.* /oem/usr/lib/
+
+adb push tts_audio/open_ir.wav /oem/ws/model
+adb push tts_audio/close_ir.wav /oem/ws/model
 adb shell reboot
 sleep 10
 cd display; python show.py
